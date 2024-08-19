b28c4cc5997c51fe0e96757afde7798fee7ca998
devendr4 <angel.emo.altuve@gmail.com>
2024-08-19 19:01:58 -0400

    install superforms

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -4,15 +4,15 @@
 
 <h1 class="text-4xl text-center">Lista de recetas</h1>
 
-<ul>
+<ul class="flex flex-col gap-4">
 	{#each data.recipes as recipe}
-		<li>
-			<div>
-				<img src={recipe.img} alt={recipe.title} />
-				<h3>
-					{recipe.title}
-				</h3>
-			</div>
+		<li class="flex items-center justify-around">
+			<a href={`/recipe/${recipe.id}`}>
+				<img height="auto" width="150" src={recipe.img} alt={recipe.title} />
+			</a>
+			<h3>
+				{recipe.title}
+			</h3>
 		</li>
 	{/each}
 </ul>
