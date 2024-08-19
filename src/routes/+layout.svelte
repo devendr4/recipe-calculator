b7962b752cd3b360904fff95af91d7906114f5a0
devendr4 <angel.emo.altuve@gmail.com>
2024-08-18 23:26:16 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="bg-purple-300 p-8 h-screen">
+	<slot />
+</div>
+<nav class="bg-purple-400 fixed w-full left-0 bottom-0">
+	<ul class="flex gap-2">
+		<li class="bg-purple-500 p-3 w-1/2 text-center">
+			<button> lista </button>
+		</li>
+		<li class="bg-purple-500 p-3 w-1/2 text-center">
+			<button> nueva receta </button>
+		</li>
+		<!-- <li class="bg-purple-500 p-3 w-1/3 text-center">nueva</li> -->
+	</ul>
+</nav>
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export interface Recipe {
+	title: string;
+	img: string;
+}
